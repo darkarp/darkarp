e7152d2bafc263d91cd92051531399954c8677cb
darkArp <22871633+darkarp@users.noreply.github.com>
2019-11-23 08:57:07 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+setup(
+  name = 'darkarp',
+  packages = ['darkarp'],
+  version = '0.1',
+  license='MIT',
+  description = 'Collection of modules by darkArp',
+  author = 'Mario Nascimento',
+  author_email = 'marionascimento@itsec.bz',
+  url = 'https://github.com/darkarp',
+  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
+  keywords = ['darkarp', 'encryption', 'malware'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
