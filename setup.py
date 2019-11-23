93fd5f49c236a2b66e2931ced6430876767a4fea
darkArp <marionascimento047@gmail.com>
2019-11-23 11:22:24 +0100

    next

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Mario Nascimento',
     author_email='marionascimento@itsec.bz',
     url='https://github.com/darkarp',
-    download_url='https://github.com/darkarp/darkarp/archive/v0.5.tar.gz',
+    download_url='https://github.com/darkarp/darkarp/archive/v0.6.tar.gz',
     keywords=['darkarp', 'encryption', 'malware'],
     install_requires=[],
     classifiers=[
